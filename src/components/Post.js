335dde4621798767860f563ae1b2afc246044b8d
Murgesh Ekunde <mekunde@gmail.com>
2023-02-23 22:30:40 +0530

    first commit

---
+++ src/components/Post.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Post() {
+  return (
+    <div className="post">
+    <div className="image">
+    <img src="https://sp-ao.shortpixel.ai/client/to_webp,q_glossy,ret_img,w_1200/https://therootdroid.com/wp-content/uploads/2022/10/Thumbnails.png" alt="" />
+    </div>
+    <div className="text">
+    <h2>Multi-Cloud Architecture Obvious Challenges and Benefits</h2>
+    <p className="info">
+      <a className="author">Murgesh Ekunde</a>
+      <time>2023-02-23 16:45</time>
+    </p>
+    <p className="summary">A multi-cloud architecture is a cloud computing environment that uses multiple clouds from different providers.</p>
+    </div>
+  </div>
+  )
+}
+
+export default Post
---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+function RegisterPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  async function register(e){
+    e.preventDefault();
+    const response = await fetch("http://localhost:4000/register",{
+        method:'POST',
+        body: JSON.stringify({username,password}),
+        headers: {'Content-Type':'application/json'}
+    });
+    if (response.status === 200){
+        alert("Successfully Registered") 
+    } else {
+        alert('Registration Failed, Try Again!')
+    }
+  }
+  return (
+    <form className="register" onSubmit={register}>
+      <h1>Register</h1>
+      <input type="text" 
+      placeholder="username" 
+      value={username} 
+      onChange={e=>setUsername(e.target.value)} 
+      />
+
+      <input type="password" 
+      placeholder="password"
+      value={password} 
+      onChange={e=>setPassword(e.target.value)} 
+      />
+      <button> Register </button>
+    </form>
+  );
+}
+
+export default RegisterPage;
